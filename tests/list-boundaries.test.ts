beaac9391cbff253b993b1e2eb9570c05236cb83
Anton Ovechkin <owanturist@gmail.com>
2021-05-30 00:23:39 +0200

    Use jest

--- tests/list-boundaries.test.ts
+++ tests/list-boundaries.test.ts
@@ -1,41 +1,39 @@
-import test from 'ava'
-
 import { ListBoundaries } from '../src/list-boundaries'
 
-test('ListBoundaries.replace', t => {
+test('ListBoundaries.replace', () => {
   const _0 = { start: 0, stop: 1 }
   const _1 = { start: 0, stop: 2 }
   const _2 = { start: 1, stop: 1 }
 
-  t.is(ListBoundaries.replace(_0, { start: 0, stop: 1 }), _0)
-  t.is(ListBoundaries.replace(_0, _1), _1)
-  t.is(ListBoundaries.replace(_0, _2), _2)
+  expect(ListBoundaries.replace(_0, { start: 0, stop: 1 })).toBe(_0)
+  expect(ListBoundaries.replace(_0, _1)).toBe(_1)
+  expect(ListBoundaries.replace(_0, _2)).toBe(_2)
 })
 
-test('ListBoundaries.limit', t => {
+test('ListBoundaries.limit', () => {
   const _0 = ListBoundaries.limit({ start: 0, stop: 0 }, 10)
-  t.deepEqual(_0, { start: 0, stop: 0 })
+  expect(_0).toEqual({ start: 0, stop: 0 })
 
   const _1 = ListBoundaries.limit({ start: -1, stop: 11 }, 10)
-  t.deepEqual(_1, { start: 0, stop: 10 })
+  expect(_1).toEqual({ start: 0, stop: 10 })
 
   const _2 = ListBoundaries.limit({ start: 3, stop: 6 }, 10, 2)
-  t.deepEqual(_2, { start: 1, stop: 8 })
+  expect(_2).toEqual({ start: 1, stop: 8 })
 
   const _3 = ListBoundaries.limit({ start: 1, stop: 9 }, 10, 2)
-  t.deepEqual(_3, { start: 0, stop: 10 })
+  expect(_3).toEqual({ start: 0, stop: 10 })
 })
 
-test('ListBoundaries.limitOverScroll', t => {
+test('ListBoundaries.limitOverScroll', () => {
   const _0 = ListBoundaries.limitOverScroll({ start: 0, stop: 0 }, 10)
-  t.deepEqual(_0, { start: 0, stop: 0 })
+  expect(_0).toEqual({ start: 0, stop: 0 })
 
   const _1 = ListBoundaries.limitOverScroll({ start: -1, stop: 11 }, 10)
-  t.deepEqual(_1, { start: 0, stop: 10 })
+  expect(_1).toEqual({ start: 0, stop: 10 })
 
   const _2 = ListBoundaries.limitOverScroll({ start: 80, stop: 99 }, 70)
-  t.deepEqual(_2, { start: 51, stop: 70 })
+  expect(_2).toEqual({ start: 51, stop: 70 })
 
   const _3 = ListBoundaries.limitOverScroll({ start: 80, stop: 99 }, 10)
-  t.deepEqual(_3, { start: 0, stop: 10 })
+  expect(_3).toEqual({ start: 0, stop: 10 })
 })
--- tests/scroll-position.test.ts
+++ tests/scroll-position.test.ts
@@ -1,5 +1,3 @@
-import test from 'ava'
-
 import {
   calcEndPosition as calcEnd,
   calcCenterPosition as calcCenter,
@@ -7,55 +5,55 @@ import {
   calcSmartPosition as calcSmart
 } from '../src/scroll-position'
 
-test('calcEndPosition', t => {
-  t.is(calcEnd(0, 0, 0), 0)
-  t.is(calcEnd(300, 150, 100), 350, 'item > container')
-  t.is(calcEnd(49, 100, 150), 0, 'container > end position')
-  t.is(calcEnd(50, 100, 150), 0, 'container == end position')
-  t.is(calcEnd(51, 100, 150), 1, 'container < end position')
-  t.is(calcEnd(1500, 100, 150), 1450)
+test('calcEndPosition', () => {
+  expect(calcEnd(0, 0, 0)).toBe(0)
+  expect(calcEnd(300, 150, 100)).toBe(350) // item > container
+  expect(calcEnd(49, 100, 150)).toBe(0) // container > end position
+  expect(calcEnd(50, 100, 150)).toBe(0) // container == end position
+  expect(calcEnd(51, 100, 150)).toBe(1) // container < end position
+  expect(calcEnd(1500, 100, 150)).toBe(1450)
 })
 
-test('calcCenterPosition', t => {
-  t.is(calcCenter(0, 0, 0), 0)
-  t.is(calcCenter(300, 150, 100), 325, 'item > container')
-  t.is(calcCenter(0, 100, 150), 0, 'container > end position')
-  t.is(calcCenter(25, 100, 150), 0, 'container == end position')
-  t.is(calcCenter(26, 100, 150), 1, 'container < end position')
-  t.is(calcCenter(1500, 100, 150), 1475)
+test('calcCenterPosition', () => {
+  expect(calcCenter(0, 0, 0)).toBe(0)
+  expect(calcCenter(300, 150, 100)).toBe(325) // item > container
+  expect(calcCenter(0, 100, 150)).toBe(0) // container > end position
+  expect(calcCenter(25, 100, 150)).toBe(0) // container == end position
+  expect(calcCenter(26, 100, 150)).toBe(1) // container < end position
+  expect(calcCenter(1500, 100, 150)).toBe(1475)
 })
 
-test('calcAutoPosition', t => {
-  t.is(calcAuto(0, 0, 0, 0), 0)
+test('calcAutoPosition', () => {
+  expect(calcAuto(0, 0, 0, 0)).toBe(0)
 
-  t.is(calcAuto(1500, 100, 150, 1500), 1500, 'visible at exact start')
-  t.is(calcAuto(1500, 100, 150, 1501), 1500, 'almost visible at start')
-  t.is(calcAuto(1500, 100, 150, 1499), 1499, 'visible at start')
+  expect(calcAuto(1500, 100, 150, 1500)).toBe(1500) // visible at exact start
+  expect(calcAuto(1500, 100, 150, 1501)).toBe(1500) // almost visible at start
+  expect(calcAuto(1500, 100, 150, 1499)).toBe(1499) // visible at start
 
-  t.is(calcAuto(1500, 100, 150, 1450), 1450, 'visible at exact end')
-  t.is(calcAuto(1500, 100, 150, 1449), 1450, 'almost visible at end')
-  t.is(calcAuto(1500, 100, 150, 1451), 1451, 'visible at end')
+  expect(calcAuto(1500, 100, 150, 1450)).toBe(1450) // visible at exact end
+  expect(calcAuto(1500, 100, 150, 1449)).toBe(1450) // almost visible at end
+  expect(calcAuto(1500, 100, 150, 1451)).toBe(1451) // visible at end
 
-  t.is(calcAuto(1500, 100, 150, 1650), 1500, '1 viewport from start')
-  t.is(calcAuto(1500, 100, 150, 1651), 1500, '> 1 viewport from start')
+  expect(calcAuto(1500, 100, 150, 1650)).toBe(1500) // 1 viewport from start
+  expect(calcAuto(1500, 100, 150, 1651)).toBe(1500) // > 1 viewport from start
 
-  t.is(calcAuto(1500, 100, 150, 1300), 1450, '1 viewport from end')
-  t.is(calcAuto(1500, 100, 150, 1299), 1450, '> 1 viewport from end')
+  expect(calcAuto(1500, 100, 150, 1300)).toBe(1450) // 1 viewport from end
+  expect(calcAuto(1500, 100, 150, 1299)).toBe(1450) // > 1 viewport from end
 })
 
-test('calcSmartPosition', t => {
-  t.is(calcSmart(0, 0, 0, 0), 0)
-  t.is(calcSmart(1500, 100, 150, 1500), 1500, 'visible at exact start')
-  t.is(calcSmart(1500, 100, 150, 1501), 1500, 'almost visible at start')
-  t.is(calcSmart(1500, 100, 150, 1499), 1499, 'visible at start')
+test('calcSmartPosition', () => {
+  expect(calcSmart(0, 0, 0, 0)).toBe(0)
+  expect(calcSmart(1500, 100, 150, 1500)).toBe(1500) // visible at exact start
+  expect(calcSmart(1500, 100, 150, 1501)).toBe(1500) // almost visible at start
+  expect(calcSmart(1500, 100, 150, 1499)).toBe(1499) // visible at start
 
-  t.is(calcSmart(1500, 100, 150, 1450), 1450, 'visible at exact end')
-  t.is(calcSmart(1500, 100, 150, 1449), 1450, 'almost visible at end')
-  t.is(calcSmart(1500, 100, 150, 1451), 1451, 'visible at end')
+  expect(calcSmart(1500, 100, 150, 1450)).toBe(1450) // visible at exact end
+  expect(calcSmart(1500, 100, 150, 1449)).toBe(1450) // almost visible at end
+  expect(calcSmart(1500, 100, 150, 1451)).toBe(1451) // visible at end
 
-  t.is(calcSmart(1500, 100, 150, 1650), 1500, '1 viewport from start')
-  t.is(calcSmart(1500, 100, 150, 1651), 1475, '> 1 viewport from start')
+  expect(calcSmart(1500, 100, 150, 1650)).toBe(1500) // 1 viewport from start
+  expect(calcSmart(1500, 100, 150, 1651)).toBe(1475) // > 1 viewport from start
 
-  t.is(calcSmart(1500, 100, 150, 1300), 1450, '1 viewport from end')
-  t.is(calcSmart(1500, 100, 150, 1299), 1475, '> 1 viewport from end')
+  expect(calcSmart(1500, 100, 150, 1300)).toBe(1450) // 1 viewport from end
+  expect(calcSmart(1500, 100, 150, 1299)).toBe(1475) // > 1 viewport from end
 })
