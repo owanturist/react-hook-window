f6239691f9253128811cebdd37b0b1849e1e23f1
Anton Ovechkin <owanturist@gmail.com>
2021-05-30 22:54:18 +0200

    Test item count change

--- cypress/integration/fixed-size-list.ts
+++ cypress/integration/fixed-size-list.ts
@@ -31,7 +31,7 @@ it('handles scrolling correctly', () => {
   cy.findByText('Item #0').should('not.exist')
   cy.findAllByTestId('item').should('have.length', 13)
 
-  // scroll to the down
+  // scroll to the very bottom
   cy.get('@container').scrollTo(0, 490, { duration: 100 })
 
   cy.findByText('Item #19').should('be.visible')
@@ -68,7 +68,7 @@ it('handles container resizing', () => {
   cy.findByText('Item #0').should('be.visible')
   cy.findAllByTestId('item').should('have.length', 6)
 
-  // scroll to the down
+  // scroll to the very bottom
   cy.get('@container').scrollTo(0, 790, { duration: 100 })
   cy.findByText('Item #19').should('be.visible')
   cy.findByText('Item #15').should('be.visible')
@@ -103,11 +103,71 @@ it('handles item resizing', () => {
   cy.findByText('Item #0').should('be.visible')
   cy.findAllByTestId('item').should('have.length', 18)
 
-  // scroll to the down
+  // scroll to the very bottom
   cy.get('@container').scrollTo(0, 90, { duration: 100 })
   cy.findByText('Item #19').should('be.visible')
   cy.findByText('Item #3').should('be.visible')
   cy.findByText('Item #2').should('be.visible')
   cy.findByText('Item #1').should('not.exist')
   cy.findAllByTestId('item').should('have.length', 18)
+})
+
+it('handle item count change', () => {
+  cy.findByTestId('container').as('container').should('be.visible')
+  cy.get('@container').getClientHeight().should('eq', 510)
+  cy.get('@container').getScrollHeight().should('eq', 1000) // 50px * 20
+
+  // decrease item count
+  cy.findByTestId('item-count-input').fill('15')
+  cy.get('@container').getClientHeight().should('eq', 510)
+  cy.get('@container').getScrollHeight().should('eq', 750) // 50px * 15
+
+  cy.findByText('Item #12').should('not.exist')
+  cy.findByText('Item #11').should('not.be.visible')
+  cy.findByText('Item #10').should('be.visible')
+  cy.findByText('Item #0').should('be.visible')
+  cy.findAllByTestId('item').should('have.length', 12)
+
+  // scroll to the very bottom
+  cy.get('@container').scrollTo(0, 240, { duration: 100 })
+  cy.findByText('Item #14').should('be.visible')
+  cy.findByText('Item #4').should('be.visible')
+  cy.findByText('Item #3').should('not.be.visible')
+  cy.findByText('Item #2').should('not.exist')
+  cy.findAllByTestId('item').should('have.length', 12)
+
+  // scroll top and increase item count
+  cy.get('@container').scrollTo(0, 0, { duration: 100 })
+  cy.findByTestId('item-count-input').fill('1000')
+  cy.get('@container').getClientHeight().should('eq', 510)
+  cy.get('@container').getScrollHeight().should('eq', 50000) // 50px * 1000
+
+  // scroll to the very bottom
+  cy.get('@container').scrollTo(0, 49490, { duration: 300 })
+  cy.findByText('Item #999').should('be.visible')
+  cy.findByText('Item #989').should('be.visible')
+  cy.findByText('Item #988').should('not.be.visible')
+  cy.findByText('Item #987').should('not.exist')
+  cy.findAllByTestId('item').should('have.length', 12)
+
+  // cause overscroll
+  cy.findByTestId('item-count-input').fill('100')
+  cy.get('@container').getClientHeight().should('eq', 510)
+  cy.get('@container').getScrollHeight().should('eq', 5000) // 50px * 100
+
+  cy.findByText('Item #99').should('be.visible')
+  cy.findByText('Item #89').should('be.visible')
+  cy.findByText('Item #88').should('not.be.visible')
+  cy.findByText('Item #87').should('not.exist')
+  cy.findAllByTestId('item').should('have.length', 12)
+
+  // cause overscroll again
+  cy.findByTestId('item-count-input').fill('10')
+  cy.get('@container').getClientHeight().should('eq', 510)
+  cy.get('@container').getScrollHeight().should('eq', 510) // 50px * 10 < 510
+
+  cy.findByText('Item #9').should('be.visible')
+  cy.findByText('Item #9').should('be.visible')
+  cy.findByText('Item #0').should('be.visible')
+  cy.findAllByTestId('item').should('have.length', 10)
 })
