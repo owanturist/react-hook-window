a413bc0bfd975f0ff1e0c6a0b028722d097b003b
Anton Ovechkin <owanturist@gmail.com>
2021-05-30 22:40:04 +0200

    Add item resizing test

--- cypress/integration/fixed-size-list.ts
+++ cypress/integration/fixed-size-list.ts
@@ -8,7 +8,7 @@ it('handles scrolling correctly', () => {
   cy.get('@container').getScrollHeight().should('eq', 1000) // 50px * 20
 
   cy.findByText('Item #12').should('not.exist')
-  cy.findByText('Item #11').should('exist').should('not.be.visible')
+  cy.findByText('Item #11').should('not.be.visible')
   cy.findByText('Item #10').should('be.visible')
   cy.findByText('Item #0').should('be.visible')
   cy.findAllByTestId('item').should('have.length', 12)
@@ -16,7 +16,7 @@ it('handles scrolling correctly', () => {
   // scroll one item down
   cy.get('@container').scrollTo(0, 55, { duration: 100 })
 
-  cy.findByText('Item #12').should('exist').should('not.be.visible')
+  cy.findByText('Item #12').should('not.be.visible')
   cy.findByText('Item #11').should('be.visible')
   cy.findByText('Item #0').should('not.be.visible')
   cy.findAllByTestId('item').should('have.length', 13)
@@ -24,7 +24,7 @@ it('handles scrolling correctly', () => {
   // scroll some items down
   cy.get('@container').scrollTo(0, 151, { duration: 100 })
 
-  cy.findByText('Item #14').should('exist').should('not.be.visible')
+  cy.findByText('Item #14').should('not.be.visible')
   cy.findByText('Item #13').should('be.visible')
   cy.findByText('Item #3').should('be.visible')
   cy.findByText('Item #2').should('not.be.visible')
@@ -34,7 +34,7 @@ it('handles scrolling correctly', () => {
   // scroll to the down
   cy.get('@container').scrollTo(0, 490, { duration: 100 })
 
-  cy.findByText('Item #19').should('exist').should('be.visible')
+  cy.findByText('Item #19').should('be.visible')
   cy.findByText('Item #9').should('be.visible')
   cy.findByText('Item #8').should('not.be.visible')
   cy.findByText('Item #7').should('not.exist')
@@ -52,7 +52,7 @@ it('handles container resizing', () => {
   cy.get('@container').getScrollHeight().should('eq', 1000) // 50px * 20
 
   cy.findByText('Item #18').should('not.exist')
-  cy.findByText('Item #17').should('exist').should('not.be.visible')
+  cy.findByText('Item #17').should('not.be.visible')
   cy.findByText('Item #16').should('be.visible')
   cy.findByText('Item #0').should('be.visible')
   cy.findAllByTestId('item').should('have.length', 18)
@@ -63,16 +63,51 @@ it('handles container resizing', () => {
   cy.get('@container').getScrollHeight().should('eq', 1000) // 50px * 20
 
   cy.findByText('Item #6').should('not.exist')
-  cy.findByText('Item #5').should('exist').should('not.be.visible')
+  cy.findByText('Item #5').should('not.be.visible')
   cy.findByText('Item #4').should('be.visible')
   cy.findByText('Item #0').should('be.visible')
   cy.findAllByTestId('item').should('have.length', 6)
 
   // scroll to the down
   cy.get('@container').scrollTo(0, 790, { duration: 100 })
-  cy.findByText('Item #19').should('exist').should('be.visible')
+  cy.findByText('Item #19').should('be.visible')
   cy.findByText('Item #15').should('be.visible')
   cy.findByText('Item #14').should('not.be.visible')
   cy.findByText('Item #13').should('not.exist')
   cy.findAllByTestId('item').should('have.length', 6)
+})
+
+it('handles item resizing', () => {
+  cy.findByTestId('container').as('container').should('be.visible')
+  cy.get('@container').getClientHeight().should('eq', 510)
+  cy.get('@container').getScrollHeight().should('eq', 1000) // 50px * 20
+
+  // expand items
+  cy.findByTestId('item-size-input').fill('90')
+  cy.get('@container').getClientHeight().should('eq', 510)
+  cy.get('@container').getScrollHeight().should('eq', 1800) // 90px * 20
+
+  cy.findByText('Item #7').should('not.exist')
+  cy.findByText('Item #6').should('not.be.visible')
+  cy.findByText('Item #5').should('be.visible')
+  cy.findByText('Item #0').should('be.visible')
+  cy.findAllByTestId('item').should('have.length', 7)
+
+  // shrink items
+  cy.findByTestId('item-size-input').fill('30')
+  cy.get('@container').getClientHeight().should('eq', 510)
+  cy.get('@container').getScrollHeight().should('eq', 600) // 30px * 20
+
+  cy.findByText('Item #18').should('not.exist')
+  cy.findByText('Item #17').should('be.visible') // visible because in the 510 border
+  cy.findByText('Item #0').should('be.visible')
+  cy.findAllByTestId('item').should('have.length', 18)
+
+  // scroll to the down
+  cy.get('@container').scrollTo(0, 90, { duration: 100 })
+  cy.findByText('Item #19').should('be.visible')
+  cy.findByText('Item #3').should('be.visible')
+  cy.findByText('Item #2').should('be.visible')
+  cy.findByText('Item #1').should('not.exist')
+  cy.findAllByTestId('item').should('have.length', 18)
 })
