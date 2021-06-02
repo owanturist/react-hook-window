57ed53de374d0780401325af4a455589b1d92425
Anton Ovechkin <owanturist@gmail.com>
2021-06-02 23:37:21 +0200

    Split FixedSizeListDemo

---
+++ demo/FixedSizeListDemo.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { useWindowedList } from '../src'
+import { DynamicScrollControl } from './ScrollControl'
+import { ControlPanelOptions, ControlPanel } from './ControlPanel'
+
+const FixedSizeList = React.memo<{
+  options: ControlPanelOptions
+}>(({ options }) => {
+  const {
+    setRef,
+    startSpace,
+    endSpace,
+    indexes,
+    isScrolling,
+    scrollTo,
+    scrollToItem
+  } = useWindowedList(options)
+
+  return (
+    <div>
+      <div>
+        Dynamic scroll
+        <DynamicScrollControl scrollTo={scrollTo} scrollToItem={scrollToItem} />
+        <input
+          data-testid="scrolling"
+          type="checkbox"
+          checked={isScrolling}
+          readOnly
+          disabled
+        />
+        Scrolling
+      </div>
+
+      <div
+        data-testid="container"
+        ref={setRef}
+        style={{
+          overflow: 'auto',
+          boxShadow: '0 0 0 2px #888',
+          height: options.containerSize
+        }}
+      >
+        <div
+          style={{
+            paddingTop: startSpace,
+            paddingBottom: endSpace
+          }}
+        >
+          {indexes.map(index => (
+            <div
+              data-testid="item"
+              key={index}
+              style={{
+                height: options.itemSize,
+                background: index % 2 === 0 ? '#eee' : '#ddd'
+              }}
+            >
+              Item #{index}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+})
+
+export const FixedSizeListDemo = React.memo(() => {
+  const [options, setOptions] = React.useState<ControlPanelOptions>({
+    containerSize: 510,
+    itemSize: 50,
+    itemCount: 20,
+    visible: true
+  })
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <ControlPanel options={options} setOptions={setOptions} />
+
+      {options.visible && <FixedSizeList options={options} />}
+    </div>
+  )
+})
