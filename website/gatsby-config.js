eb00b4f1843c384f5942da98741186160998588f
Anton Ovechkin <owanturist@gmail.com>
2021-07-04 11:25:16 +0200

    Setup website lint

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   plugins: [
     {
-      resolve: "smooth-doc",
+      resolve: 'smooth-doc',
       options: {
-        name: "Smooth DOC Starter",
-        description: "Use your own description...",
-        siteUrl: "https://example.com",
-      },
-    },
-  ],
-};
+        name: 'Smooth DOC Starter',
+        description: 'Use your own description...',
+        siteUrl: 'https://example.com'
+      }
+    }
+  ]
+}
